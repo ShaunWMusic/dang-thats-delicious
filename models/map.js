a438c1b5764da79b4fba54cac64b84079ae62169
“Titan-SWillis” <shaunw@titanwms.com>
2017-09-14 22:03:25 -0500

    Begin Launch

---
+++ models/map.js
@@ -0,0 +1,12 @@
+// for fetch ajaxs
+import axios from 'axios';
+
+function loadPlaces(map, lat= 43.2, lng = -79.8) {
+
+}
+
+function makeMap(mapDiv) {
+  console.log(mapDiv);
+}
+
+export default makeMap;
